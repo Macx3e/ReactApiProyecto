56f50159edac9d69b8549eceddf7f43eef9fec46
Maximiliano Valdivia <Valdiviamaximiliano20@gmail.com>
2025-03-09 23:27:54 -0300

    Subida e instalacion de las dependencias que se utilizaran, se agrega contenido a principal.jsx

---
+++ src/principal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Aplicacion from './Aplicacion';
+import './estilos/estilos.css'; // Aquí estarán tus estilos CSS
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Aplicacion />
+  </React.StrictMode>
+);
